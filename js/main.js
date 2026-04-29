ac3bfce0af6116aa373fc34495c9069e69721d91
Siyabonga-Zindela <siyabongazindela@outlook.com>
2026-04-29 18:43:06 +0200

    Fetched movie data from an api

--- js/main.js
+++ js/main.js
@@ -1,33 +1,56 @@
-/*const url = 'https://streaming-availability.p.rapidapi.com/shows/%7Btype%7D/%7Bid%7D';
 const options = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': '345a38d621msh2c6a1b8a80b1364p14faefjsn4fde8af0a9a1',
-		'x-rapidapi-host': 'streaming-availability.p.rapidapi.com',
+		'x-rapidapi-host': 'imdb236.p.rapidapi.com',
 		'Content-Type': 'application/json'
 	}
 };
 
-fetch(url,options)
-	.then((response => {
-		response.json
-		console.log(response);
+
+function fetchMovieData(){
+	let eachMovieHTML= "";
+	fetch("https://imdb236.p.rapidapi.com/api/imdb/cast/nm0000190/titles",options)
+		.then((response => response.json()))
+		.then((data => {
+		const movieData = data;
+		
+		if(movieData){
+			movieData.forEach(movie => {
+				if(movie.primaryImage || movie.description){
+					eachMovieHTML += `
+				 <div class="movie-card">
+          				<div class="image-container">
+           					 <img src="${movie.primaryImage}" alt="Movie cover" />
+            				<div class="movie-description">
+								<p>
+									${movie.description}
+								</p>
+              				      <div class="card-btn-wrapper"><button>Watch movie</button></div>
+           				     </div>
+          				</div>
+        		 </div>
+				`;
+				}
+				
+			});
+			
+		}
+		else{
+			alert("Unable to fetch movie data.");
+		}
+		
+		let moviesWrapper = document.querySelector(".movies-wrapper");
+
+		if(moviesWrapper){
+		moviesWrapper.innerHTML = eachMovieHTML;
+		}
+		
 	}))
-*/	
-const url = 'https://streaming-availability.p.rapidapi.com/shows/%7Btype%7D/%7Bid%7D';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '345a38d621msh2c6a1b8a80b1364p14faefjsn4fde8af0a9a1',
-		'x-rapidapi-host': 'streaming-availability.p.rapidapi.com',
-		'Content-Type': 'application/json'
-	}
-};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+	
 }
+fetchMovieData();
+
+	
+	
